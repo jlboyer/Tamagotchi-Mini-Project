0930625be0ff88bc21159f07c21fd55deaf5b1ff
jlboyer <jlboyer1@gmail.com>
2019-07-02 15:21:33 -0500

    birthtime logic and data in tomogatchi instead of game

--- js/app.js
+++ js/app.js
@@ -1,45 +1,46 @@
 class Tomogatchi {
-  constructor(){
-    this.name = ''
-    this.hunger = Math.floor(Math.random()*10 + 1)
-    this.sleepiness = Math.floor(Math.random()*10 + 1)
-    this.boredom = Math.floor(Math.random()*10 + 1)
-    this.age = 0
+  constructor() {
+    this.name = "";
+    this.hunger = Math.floor(Math.random() * 10 + 1);
+    this.sleepiness = Math.floor(Math.random() * 10 + 1);
+    this.boredom = Math.floor(Math.random() * 10 + 1);
+    this.age = 0;
+    this.birthTime = {
+      year: null,
+      month: null,
+      day: null,
+      hours: null,
+      minutes: null
+    };
+    
   }
-
+  birthday() {
+    const birthTime = new Date();
+    this.birthTime.year = birthTime.getFullYear();
+    this.birthTime.month = birthTime.getMonth();
+    this.birthTime.day = birthTime.getDay();
+  }
 }
 
 const game = {
   tomogotchi: {},
-  birthTime: {
-    year: null,
-    month: null,
-    day: null,
-    hours: null,
-    minutes: null
-  },
-  play(){
+  play() {
     //this will cycle through operations
     //1. create tomogotchi
-    this.spawnTomogotchi()
-    this.startTimer()
+    this.spawnTomogotchi();
+    this.startTimer();
   },
-  spawnTomogotchi(){
-    this.tomogotchi = new Tomogatchi
-    this.tomogotchi.birthday()
+  spawnTomogotchi() {
+    this.tomogotchi = new Tomogatchi();
+    this.tomogotchi.birthday();
   },
-  birthday(){
-    const birthTime = new Date()
-    this.birthTime.year = birthTime.getFullYear()
-    this.birthTime.month = birthTime.getMonth()
-    this.birthTime.day = birthTime.getDay()
-    
+  startTimer() {
+    let tomogotchi
     setInterval(() => {
-      console.log(new Date())
-    }, 60*1000);
-  },
+      console.log(new Date());
+    }, 60 * 1000);
+  }
+};
 
-}
-
-game.play()
-console.log(game.tomogotchi)
+game.play();
+console.log(game.tomogotchi);
