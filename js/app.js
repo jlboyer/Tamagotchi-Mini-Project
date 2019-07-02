816f59e19f2c104a3fcd6c3e0b554c106ea0dbb6
jlboyer <jlboyer1@gmail.com>
2019-07-02 15:58:41 -0500

    working on "metabolism" timing aging

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ class Tomogatchi {
     this.hunger = Math.floor(Math.random() * 10 + 1);
     this.sleepiness = Math.floor(Math.random() * 10 + 1);
     this.boredom = Math.floor(Math.random() * 10 + 1);
-    this.age = 0;
+    this.ageTomoYears = 0;
+    this.ageHumanYears = 0;
+    this.metabolism = 1; //default 1 year is 1 minute
     this.birthTime = {
       year: null,
       month: null,
@@ -12,20 +14,25 @@ class Tomogatchi {
       hours: null,
       minutes: null
     };
-    this.timerId
+    this.timerId 
   }
   birthday() {
     const birthTime = new Date();
     this.birthTime.year = birthTime.getFullYear();
     this.birthTime.month = birthTime.getMonth();
     this.birthTime.day = birthTime.getDay();
+    this.lifeTimer()
   }
   lifeTimer() {
-    tomogotchi = setInterval(() => {
-      console.log(new Date());
+    this.timerId = setInterval(() => {
+
+      Math.floor()
+      this.ageTomoYears++
     }, 60 * 1000);
   }
 }
+
+const intId = setInterval(() => console.log(intId), 1000);
 
 const game = {
   tomogotchi: {},
