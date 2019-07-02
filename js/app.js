d41ca219ab14b4b174ff7bdf6c01dc65e7d59dd8
jlboyer <jlboyer1@gmail.com>
2019-07-02 15:05:28 -0500

    working on birthday, switching repos

--- js/app.js
+++ js/app.js
@@ -11,14 +11,34 @@ class Tomogatchi {
 
 const game = {
   tomogotchi: {},
+  birthTime: {
+    year: null,
+    month: null,
+    day: null,
+    hours: null,
+    minutes: null
+  },
   play(){
     //this will cycle through operations
     //1. create tomogotchi
     this.spawnTomogotchi()
+    this.startTimer()
   },
   spawnTomogotchi(){
     this.tomogotchi = new Tomogatchi
-  }
+    this.tomogotchi.birthday()
+  },
+  birthday(){
+    const birthTime = new Date()
+    this.birthTime.year = birthTime.getFullYear()
+    this.birthTime.month = birthTime.getMonth()
+    this.birthTime.day = birthTime.getDay()
+    
+    setInterval(() => {
+      console.log(new Date())
+    }, 60*1000);
+  },
+
 }
 
 game.play()
